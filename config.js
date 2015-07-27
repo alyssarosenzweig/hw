03ba02d8f4e89ae8bd5e2263c1165fdd3024451d
Alyssa Rosenzweig <bobbybee@users.noreply.github.com>
2015-07-27 17:59:21 -0400

    Don't force markdown

--- config.js
+++ config.js
@@ -5,5 +5,7 @@ module.exports = {
         return d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
     },
     browser: "firefox",
-    useGit: "true"
+    useGit: true,
+    defaultFormat: "markdown",
+    noteFormat: "markdown"
 }
